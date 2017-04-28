53535b487a8cd0327b48befc773b627b380ea360
bannzai <hiroseyuudai@hiroseyudais-macbookpro.local>
2017-04-28 11:22:55 +0900

    Initial Commit

---
+++ PromiseDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PromiseDemo
+//
+//  Created by 廣瀬雄大 on 2017/04/28.
+//  Copyright © 2017年 廣瀬雄大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
